f783fe6ed9b7e0023fa29e7010d004174188ded1
David Antoon <david.antoon@hotmail.com>
2016-09-07 20:47:35 +0300

    Init

---
+++ app/src/main/java/net/kiwiapps/framwork7/MainActivity.java
@@ -0,0 +1,23 @@
+package net.kiwiapps.framwork7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.apptest);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        myWebView.loadUrl("file:///android_asset/index.html");
+    }
+}
